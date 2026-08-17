53c217c77fa84b2f12ede9a7c7b55057b0220575
Gianinil <lucasgianini2006@gmail.com>
2026-08-17 19:28:03 -0300

    Operadores de atribuicao

---
+++ Projeto4/Projeto4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 10;
+        Console.WriteLine(a);
+
+        a += 2;
+        Console.WriteLine(a);
+
+        int b = 10;
+        Console.WriteLine(b);
+
+        b -= 2;
+        Console.WriteLine(b);
+
+        a *= 3;
+        Console.WriteLine(a);
+
+        string s = "abc";
+        s += "def";
+        Console.WriteLine(s);
+
+        a++;
+        Console.WriteLine(a);
+
+        a--;
+        Console.WriteLine(a);
+
+        a = 0;
+        int c = a++;
+        Console.WriteLine(a);
+        Console.WriteLine(c);
+
+        a = 0;
+        c = ++a;
+        Console.WriteLine(a);
+        Console.WriteLine(c);
+
+    }
+}
+
