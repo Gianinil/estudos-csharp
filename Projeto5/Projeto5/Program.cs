70c5dcbc2d49be4575ec3c786f2b121988f37742
Gianinil <lucasgianini2006@gmail.com>
2026-08-18 20:02:21 -0300

    conversao explicita e implicita

---
+++ Projeto5/Projeto5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Projeto5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //conversão implícita
+            float x = 4.5f;
+            double y = x;
+
+            Console.WriteLine(y);
+
+            double a;
+            float b;
+            int c;
+
+            a = 5.1;
+            b = (float)a; //conversão explícita (cast)
+
+            Console.WriteLine(b);
+
+            c = (int)a; 
+            Console.WriteLine(c); 
+
+            int d = 5;
+            int e = 2;
+
+            double resultado = d / e; //divisão inteira, resultado = 2
+            Console.WriteLine(resultado);
+
+            resultado = (double) d / e;
+            Console.WriteLine(resultado);
+        }
+    }
+}
