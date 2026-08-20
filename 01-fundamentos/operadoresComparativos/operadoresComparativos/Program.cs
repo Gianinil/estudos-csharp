fc80d9bfe1e350a70efe0aeddd24eca531d7ee7e
Gianinil <lucasgianini2006@gmail.com>
2026-08-20 20:50:00 -0300

    Operadores comparativos

---
+++ 01-fundamentos/operadoresComparativos/operadoresComparativos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace operadoresComparativos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+
+            bool b = a == 1;
+            bool c = a != 1;
+            bool d = a > 1;
+            bool e = a < 1;
+            bool f = a >= 1;
+            bool g = a <= 1;
+
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+        }
+    }
+}
