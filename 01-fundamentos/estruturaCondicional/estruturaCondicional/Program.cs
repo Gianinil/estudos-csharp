f1bd09ccdb350965c96117c411f3d82a7cd3c085
Lukotag <lukota2006@gmail.com>
2026-08-24 18:36:55 -0300

    estrutura condicional

---
+++ 01-fundamentos/estruturaCondicional/estruturaCondicional/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace estruturaCondicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+
+            if (x > 9)
+            {
+                Console.WriteLine("Boa tarde!");
+            }
+
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("Numero é Par!");
+            }
+
+            else
+            {
+                Console.WriteLine("Numero é Impar!");
+            }
+
+            Console.WriteLine("Digite o horário");
+
+            int horario = int.Parse(Console.ReadLine());
+
+            if (horario < 12)
+            {
+                Console.WriteLine("Bom dia!");
+            }
+
+            else if (horario < 18)
+            {
+                Console.WriteLine("Boa tarde!");
+            }
+
+            else
+            {
+                Console.WriteLine("Boa noite!");
+            }
+        }
+    }
+}
