4654db665c7f5e320f04c1371a59234f3d4342db
Gianinil <lucasgianini2006@gmail.com>
2026-08-28 20:11:53 -0300

    ex de media

---
+++ 03-exercicios/ex03/ex03/Funcionarios.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex03
+{
+    internal class Funcionarios
+    {
+        public string nome;
+        public double salario;
+
+    }
+}
---
+++ 03-exercicios/ex03/ex03/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Funcionarios f1 = new Funcionarios();
+            Funcionarios f2 = new Funcionarios();
+
+            Console.WriteLine("dados do funcionario 1: ");
+            Console.Write("nome: ");
+            f1.nome = Console.ReadLine();
+
+            Console.Write("salario: ");
+            f1.salario = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("dados do funcionario 2: ");
+            Console.Write("nome: ");
+            f2.nome = Console.ReadLine();
+
+            Console.Write("salario: ");
+            f2.salario = double.Parse(Console.ReadLine());
+
+            double media = (f1.salario + f2.salario) / 2.0;
+
+            Console.WriteLine($"media dos salarios: {media:F2}");
+
+        }
+    }
+
+}
