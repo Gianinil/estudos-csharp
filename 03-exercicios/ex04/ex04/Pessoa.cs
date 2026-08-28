9a57af07d8d34e5bfae72331a49c9651750522a0
Gianinil <lucasgianini2006@gmail.com>
2026-08-28 20:26:57 -0300

    ex poo com idade

---
+++ 03-exercicios/ex04/ex04/Pessoa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex04
+{
+    internal class Pessoa
+    {
+        public string nome;
+        public int idade;
+    }
+}
---
+++ 03-exercicios/ex04/ex04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ex04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa();
+
+            Console.WriteLine("Digite o nome da primeira pessoa: ");
+            Console.Write("nome: ");
+            p1.nome = Console.ReadLine();
+            Console.Write("idade: ");
+            p1.idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o nome da segunda pessoa: ");
+            Console.Write("nome: ");
+            p2.nome = Console.ReadLine();
+            Console.Write("idade: ");
+            p2.idade = int.Parse(Console.ReadLine());
+
+            if (p1.idade > p2.idade) {
+                Console.WriteLine($"Pessoa mais velha: {p1.nome}");
+            } 
+            else {
+                Console.WriteLine($"Pessoa mais velha: {p2.nome}");
+            }
+
+
+        }
+    }
+}
