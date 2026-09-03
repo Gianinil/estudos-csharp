d75065ba6fe8ccaefe398b7e2d13d207a35af704
Gianinil <lucasgianini2006@gmail.com>
2026-09-03 16:40:38 -0300

    exercicio conversao do dolar descontando o IOF

---
+++ 03-exercicios/ex08/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ex08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a cotacao do Dollar atualmente: ");
+            double dolar = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quantos Dollares voce deseja comprar em R$: ");
+            double quantidade = double.Parse(Console.ReadLine());
+            
+            double total = quantidade / dolar;
+
+            double iof = 6.0;
+            
+            double valorTotal = total * (1 - (iof / 100));
+            
+
+            Console.WriteLine($"{total:F2} {quantidade} {valorTotal:F2}");
+
+        }
+    }
+}
