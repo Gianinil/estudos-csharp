6822d30b387ef68c2fa61949f5fbd1238ef6a6f4
Gianinil <lucasgianini2006@gmail.com>
2026-09-16 12:32:09 -0300

    exercicios

---
+++ 01-fundamentos/Listas/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using  System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> list = new List<string>();
+        
+        List<string> list2 = new List<string> { "Maria", "Alex" };
+    }
+}
---
+++ 03-exercicios/ex14/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> list = new List<string>();
+
+        list.Add("Maria");
+        list.Add("Alex");
+        list.Add("Bob");
+        list.Add("Ana");
+
+        foreach (string obj in list)
+        {
+            Console.WriteLine(obj);
+        }
+        
+        Console.WriteLine("List count: " + list.Count);
+
+        Console.WriteLine("_________________________________________");
+        
+        list.Insert(0, "Bob");
+
+        foreach (string obj in list)
+        {
+            Console.WriteLine(obj);
+        }
+
+        Console.WriteLine("List count: " + list.Count);
+
+        Console.WriteLine();
+
+        string s1 = list.Find(Test);
+        Console.WriteLine("First 'A': " + s1);
+        
+        string s2 = list.Find(x => x[0] == 'B');
+        Console.WriteLine("First 'B': " + s2);
+        
+        string s3 = list.FindLast(x => x[0] == 'A');
+        Console.WriteLine("Last 'A': " + s3);
+        
+    }
+
+    static bool Test(string s)
+    {
+        return s[0] == 'A';
+    }
+}
