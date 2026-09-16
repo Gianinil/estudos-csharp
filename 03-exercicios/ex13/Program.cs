e2adf369b0117481ecf8f7e8d2096b633c10564b
Gianinil <lucasgianini2006@gmail.com>
2026-09-15 21:35:18 -0300

    exercicios

---
+++ 03-exercicios/ex13/Program.cs
@@ -0,0 +1,13 @@
+﻿using  System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] vect = new string[] { "Maria", "Alex", "Bob" };
+
+        foreach (string obj in vect)
+        {
+            Console.WriteLine(obj);
+        }
+    }
+}
