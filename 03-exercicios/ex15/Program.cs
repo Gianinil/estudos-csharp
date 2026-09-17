86c88239328c73e96635060d50ff60c216fa4745
Gianinil <lucasgianini2006@gmail.com>
2026-09-17 19:07:19 -0300

    exercicios

---
+++ 03-exercicios/ex15/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace  ex15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+        }
+    }
+}
+
